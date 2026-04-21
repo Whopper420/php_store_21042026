447203c62ece9074d7839fe5418247bcfd2ad3d4
Pepsik Men <you@example.com>
2026-04-21 13:14:01 +0300

    added /customers to route to shiwng customers

--- db/connect.php
+++ db/connect.php
@@ -4,12 +4,10 @@ $username = "store_app";
 $password = "password";
 $dbname = "store_dev";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$pdo = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if ($pdo->connect_error) {
+  die("Connection failed: " . $pdo->connect_error);
 }
 echo "Connected successfully";
 ?>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,43 @@
 <?php
 
-echo "<h1>Veikals</h1>";
+require __DIR__ . '/../db/connect.php';
+
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($requestUri === '/customers') {
+
+    $result = $pdo->query("SELECT * FROM customers");
+
+    echo "<h1>Klienti</h1>";
+
+    while ($c = $result->fetch_assoc()) {
+        echo "<p>{$c['first_name']} {$c['last_name']} ({$c['email']})</p>";
+    }
+
+} else {
+    echo "<h1>Veikals</h1>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
