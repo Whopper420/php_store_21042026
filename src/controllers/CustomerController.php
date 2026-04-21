ef9b290f2548decf3322b60cc07e4d75fba13047
Pepsik Men <you@example.com>
2026-04-21 13:29:31 +0300

    very minute changes to the customers controller for more understandale code reading

--- src/controllers/CustomerController.php
+++ src/controllers/CustomerController.php
@@ -4,14 +4,18 @@ require_once __DIR__ . '/../../db/DB.php';
 
 class CustomerController
 {
-    public static function index($pdo)
+    public static function index()
     {
         $result = DB::query("SELECT * FROM customers");
 
         echo "<h1>Klienti</h1>";
 
         while ($c = $result->fetch_assoc()) {
-            echo "<p>{$c['first_name']} {$c['last_name']} ({$c['email']})</p>";
+            $first = htmlspecialchars($c['first_name']);
+            $last  = htmlspecialchars($c['last_name']);
+            $email = htmlspecialchars($c['email']);
+
+            echo "<p>{$first} {$last} ({$email})</p>";
         }
     }
 }
